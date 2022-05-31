587bd0166aaefdbb2affaed893ccec72ba478f8d
K Sandeep Reddy <sandeepreddykothakota08@gmail.com>
2022-05-31 20:33:18 +0530

    input field of description overflow problem fixed

--- src/Components/AddToDo.js
+++ src/Components/AddToDo.js
@@ -23,12 +23,12 @@ export default function AddToDo({addToDo}) {
     <div>
         <form className="ml-3 mt-2" onSubmit={submit}>
             <div className="mb-3 flex justify-between">
-              <label htmlFor="Title" className="form-label">Title:</label>
-              <input type="text" value={Title} onChange={(e)=>setTitle(e.target.value)} className="mr-2 rounded-lg py-0 px-3" id="Title" aria-describedby="emailHelp"/>
+              <label htmlFor="Title" className="text-lg">Title:</label>
+              <input type="text" value={Title} onChange={(e)=>setTitle(e.target.value)} className="mr-2 rounded-lg py-0 px-2 w-[65%]" id="Title" aria-describedby="emailHelp"/>
             </div>
             <div className="mb-3 flex justify-between">
-              <label htmlFor="Description" className="form-label">Description:</label>
-            <input type="text" value={Description} onChange={(e)=>setDescription(e.target.value)} className="mr-2 rounded-lg py-0 px-3" id="Description"/>
+              <label htmlFor="Description" className="text-lg">Description:</label>
+            <input type="text" value={Description} onChange={(e)=>setDescription(e.target.value)} className="mr-2 w-[65%] rounded-lg py-0 px-3" id="Description"/>
             </div>
             <button type="submit" className="p-1 bg-emerald-500 font-semibold rounded-lg active:bg-emerald-400 mb-1">Add Task</button>
         </form>
